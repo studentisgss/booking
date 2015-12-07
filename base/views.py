f100d48e6402411d972120de1f17ceb57ad3c453
Daniele Troletti <dan.troletti@gmail.com>
2015-12-07 23:41:07 +0100

    Create app base

---
+++ base/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.utils import timezone
+
+
+class BookingTemplateView(TemplateView):
+
+    template_path = ""
+
+    def get_template_names(self):
+        return [self.template_path + x
+                for x in super(BookingTemplateView, self).get_template_names()]
+
+    def localnow():
+        return timezone.localtime(timezone.now(),
+                                  timezone.get_default_timezone())
+
+
+class BaseBookingTemplateView(BookingTemplateView):
+
+    template_path = "base"
