f4106a6bfdf3ea67eee72c71c0c65bc8b25a3ac6
Daniele Troletti <dan.troletti@gmail.com>
2015-12-08 15:49:01 +0100

    Add base layout
    See: #9

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.views.generic import TemplateView
 
 
 class BookingTemplateView(TemplateView):
@@ -17,4 +18,4 @@ class BookingTemplateView(TemplateView):
 
 class BaseBookingTemplateView(BookingTemplateView):
 
-    template_path = "base"
+    template_path = "base/"
