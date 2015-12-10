5ee6a0ce05af6ffddb3e944d023cc6e03a2cfc7a
Federico Poli <federpoli@gmail.com>
2015-12-10 21:58:38 +0100

    Add test for template_path

--- base/tests.py
+++ base/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.views.generic import TemplateView
+from base.views import GenericTemplateView
 
-# Create your tests here.
+
+class TemplatePathTestCase(TestCase):
+    def setUp(self):
+        class A(GenericTemplateView):
+            pass
+
+        class B(A):
+            template_path = "template/path/"
+
+        class C(B):
+            template_name = "path/to/page.html"
+
+        self.c_object = C()
+
+    def test_get_template_names(self):
+        """
+        Test that C.get_template_names() returns the expected path
+        """
+        self.assertEqual(
+            self.c_object.get_template_names(),
+            ["template/path/path/to/page.html"]
+        )
