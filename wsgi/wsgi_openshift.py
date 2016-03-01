a5feac941bce335984a5853ace749909d84414c4
Federico Poli <federpoli@gmail.com>
2016-03-02 00:04:34 +0100

    Work on Openshift deploy

---
+++ wsgi/wsgi_openshift.py
@@ -0,0 +1,19 @@
+import sys
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "booking.settings")
+
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+
+try:
+    exec_namespace = dict(__file__=virtualenv)
+    with open(virtualenv, 'rb') as exec_file:
+        file_contents = exec_file.read()
+    compiled_code = compile(file_contents, virtualenv, 'exec')
+    exec(compiled_code, exec_namespace)
+except IOError:
+    pass
+
+application = get_wsgi_application()
