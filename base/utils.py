0b7c270d93ea8082bf8e1ef45f3acbcf8f894d1f
Roberto Albesiano <roberto.albesiano@gmail.com>
2016-04-05 22:59:50 +0200

    Add default_datetime

--- base/utils.py
+++ base/utils.py
@@ -1,4 +1,5 @@
 from django.utils import timezone
+from datetime import datetime
 
 # Common utilities
 
@@ -8,3 +9,9 @@ def localnow():
     Return a datetime object representing the time in the local timezone
     """
     return timezone.localtime(timezone.now(), timezone.get_default_timezone())
+
+def default_datetime(*args, **kwargs):
+    if 'tzinfo' not in kwargs:
+        return datetime(*args, tzinfo=timezone.get_default_timezone(), **kwargs)
+    else:
+        return datetime(*args, **kwargs)
