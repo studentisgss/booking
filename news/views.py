e17e06d2e0e52140851b785f54a4c08ba8f8f5cf
Soldà Federico <federicosold@outlook.com>
2016-05-03 23:22:16 +0200

    Create news page

--- news/views.py
+++ news/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from news.models import News
+
 # Create your views here.
+
+
+class NewsView(TemplateView):
+    template_name = "news/list.html"
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        news = News.objects.all().order_by('-start')
+        context["news_list"] = news
+        return context
+
