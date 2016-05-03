1318e414bfd4756d086ad1cc5d77027538551a7e
Soldà Federico <federicosold@outlook.com>
2016-05-03 23:26:33 +0200

    Correct white spaces

--- news/views.py
+++ news/views.py
@@ -9,9 +9,8 @@ from news.models import News
 class NewsView(TemplateView):
     template_name = "news/list.html"
 
-    def get_context_data(self,**kwargs):
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         news = News.objects.all().order_by('-start')
         context["news_list"] = news
         return context
-
