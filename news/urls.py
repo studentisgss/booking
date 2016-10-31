e5cde0653094b4919243c45643986202a0d78f4d
Daniele Troletti <dan.troletti@gmail.com>
2016-10-31 16:12:28 +0100

    Add news edit page

--- news/urls.py
+++ news/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
-from news.views import NewsView
+from news.views import NewsView, NewsEditView
 
 app_name = "news"
 
 urlpatterns = [
-    url(r'^$', NewsView.as_view(), name="news")
+    url(r'^$', NewsView.as_view(), name="news"),
+    url(r'^new$', NewsEditView.as_view(), name="new"),
+    url(r'^edit/(?P<pk>\d+)$', NewsEditView.as_view(), name="edit"),
 ]
--- news/views.py
+++ news/views.py
@@ -1,6 +1,9 @@
 from django.views.generic import TemplateView
+from django.http import Http404, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 from news.models import News
+from news.forms import NewsForm
 
 
 class NewsView(TemplateView):
@@ -11,3 +14,49 @@ class NewsView(TemplateView):
         news = News.objects.all().order_by("-start")
         context["news_list"] = news
         return context
+
+
+class NewsEditView(TemplateView):
+    template_name = "news/edit.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Http method
+        if self.request.method == "GET":
+            if "pk" in kwargs and kwargs["pk"]:
+                try:
+                    news = News.objects.all().get(pk=kwargs["pk"])
+                except:
+                    raise Http404
+                form = NewsForm(instance=news)
+                context["edit"] = True
+            else:
+                form = NewsForm()
+        elif self.request.method == "POST":
+            form = NewsForm(self.request.POST)
+            context["edit"] = kwargs["edit"]
+        else:
+            raise Http404
+        context["form"] = form
+        return context
+
+    def post(self, request, *args, **kwargs):
+        if "pk" in kwargs and kwargs["pk"]:
+            try:
+                news = News.objects.all().get(pk=kwargs["pk"])
+            except:
+                raise Http404
+            form = NewsForm(request.POST, instance=news)
+            kwargs["edit"] = True
+        else:
+            form = NewsForm(request.POST)
+            kwargs["edit"] = False
+
+        if form.is_valid():
+            news = form.save(commit=False)
+            news.creator = request.user
+            news.save()
+            return HttpResponseRedirect(reverse("news:news"))
+        else:
+            return self.get(request, *args, **kwargs)
+
