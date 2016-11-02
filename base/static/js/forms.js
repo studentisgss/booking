84d4522b42fd1d67f9a9404abdf3e7d78ebd5e2a
Daniele Troletti <dan.troletti@gmail.com>
2016-11-02 22:50:22 +0100

    Add placeholder to date input

--- base/static/js/forms.js
+++ base/static/js/forms.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     $(".date").datepicker();
+    $(".date").attr("placeholder", "gg/mm/aaaa");
     // A datetime field has two input: the first is a date, the second is a time
     $(".datetime:even").datepicker();
-    $(".datetime:odd").attr("placeholder", "hh:mm:ss")
+    $(".datetime:even").attr("placeholder", "gg/mm/aaaa");
+    $(".datetime:odd").attr("placeholder", "hh:mm:ss");
 });
