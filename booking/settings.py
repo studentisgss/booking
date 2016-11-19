2f597066fbd96b69f0758ef8d5f9e1ea593f107e
Daniele Troletti <dan.troletti@gmail.com>
2016-11-19 18:00:33 +0100

    Add LOGIN_URL in settings

--- booking/settings.py
+++ booking/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from django.core.urlresolvers import reverse_lazy
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -29,6 +30,7 @@ DEMO = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_URL = reverse_lazy("admin:login")
 
 # Application definition
 
