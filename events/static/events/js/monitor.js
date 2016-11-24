ee37b00f34cffd4c29cdf0922a1a146ea205e13b
Daniele Troletti <dan.troletti@gmail.com>
2016-11-24 16:35:06 +0100

    Adjust table width in monitor page

--- events/static/events/js/monitor.js
+++ events/static/events/js/monitor.js
@@ -1,5 +1,6 @@
 function adjustIndicatorWidth()
 {
+	// Set carousel indicator width
 	var array = $(".item table").map(function(){ 
 		var clone = $(this).clone();
 		clone.css("visibility","hidden");
@@ -10,6 +11,20 @@ function adjustIndicatorWidth()
 	});
 	var width = Math.max(...array);
 	$(".carousel-indicators").css("width", width + "px");
+
+	// Set all table to the same width
+	// but do not chenge thw width of the first cell
+	$.each($(".carousel td:not(.head):even"), function(){ 
+		var clone = $(this).clone();
+		clone.css("visibility","hidden");
+    	$('body').append(clone);
+		$(this).data("width", clone.css("width"));
+		clone.remove();
+	});
+	$(".carousel table").css("width", width + "px");
+	$.each($(".carousel td:not(.head):even"), function(){ 
+		$(this).css("width", $(this).data("width"));
+	});
 }
 
 $(document).ready(function() {
