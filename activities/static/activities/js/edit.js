52bfe6e39a6110ac4393830301602be178701822
Daniele Troletti <dan.troletti@gmail.com>
2016-11-28 17:55:01 +0100

    Add repeat function in edit activity

--- activities/static/activities/js/edit.js
+++ activities/static/activities/js/edit.js
@@ -1,3 +1,6 @@
+// Variable for repeat function
+var repeatTarget = null;
+
 $(document).ready(function() {
 	// Highlight event which cannot be approved
 	$("select.form-control").on("change", function(){
@@ -29,14 +32,89 @@ $(document).ready(function() {
 		$(".datetime:even", tr).val($(this).val());
 	});
 
-	// Add button
-	$("#add-button").on("click", function(){
+	function AddForm(element) {
 		var id = $("#id_event_set-TOTAL_FORMS").val();
-		$("#id_event_set-TOTAL_FORMS").val(id + 1);
-		var el = $($('#tr-empty')[0].outerHTML.replace(/__prefix__/g, id));
+		$("#id_event_set-TOTAL_FORMS").val(parseInt(id) + 1);
+		var el = $($(element)[0].outerHTML.replace(/__prefix__/g, id));
+		el.removeAttr("id");
 		el.insertBefore($('#tr-button'));
 		el.removeClass("hidden");
 		$(".datetime:even", el).removeClass("hasDatepicker")
 		$(".datetime:even", el).datepicker();
+		return el;
+	}
+
+	// Add button
+	$("#add-button").on("click", function(){
+		AddForm($('#tr-empty'));
+	});
+
+	//REPEAT FUNCTION
+	$("button[data-target=\"#repeat-modal\"]").removeClass("hidden");
+
+	function clearModal(){
+		var modal = $("#repeat-modal");
+		$("input:not([type=\"checkbox\"])", modal).val("");
+		$("input[type=\"checkbox\"]", modal).prop("checked", false);
+	}
+
+	$("#repeat-modal").on("show.bs.modal", function(e){
+		clearModal();
+		var btn = $(e.relatedTarget);
+		var tr = btn.parent().parent();
+		repeatTarget = tr;
+		var date = $(".datetime:first", tr).val();
+		if (date){
+			dateSplit = date.split("/");
+			var d = new Date(dateSplit[2], dateSplit[1] - 1, dateSplit[0]);
+			d.setDate(d.getDate() + 1);
+			$("#date-from", $(this)).val(d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear());
+		}
+	});
+
+	$("#btn-repeat").on("click", function(){
+		var modal = $("#repeat-modal");
+		var days = $("input[name='day']:checked", modal).map(function(index,domElement) {
+		    return parseInt($(domElement).val());
+		}).get();
+		days.sort();
+		var startDate = $("#date-from", modal).val();
+		var times = $("#repeat-times", modal).val();
+		if (!(days.length > 0 && startDate && times)){
+			// FEEDBACK
+			alert("ERROR");
+			return;
+		}
+
+		var dateSplit = startDate.split("/");
+		var date = new Date(dateSplit[2], dateSplit[1] - 1, dateSplit[0]);
+
+		// Get the first day greater or equal than date in days
+		while (days.indexOf(date.getDay()) == -1) {
+			date.setDate(date.getDate() + 1);
+		}
+
+		while (times > 0) {
+			// Add the new event
+			var event = AddForm($('#tr-empty'))
+			event.removeAttr("id");
+			$("select", event).val($("select", repeatTarget).val());
+			$.each($(".datetime:odd", event), function(i, value){
+				$(value).val($(".datetime:odd", repeatTarget)[i].value);
+			});
+			$.each($(".datetime:even", event), function(i, value){
+				$(value).val(date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear());
+			});
+
+			// Find next date and decrement times
+			times -= 1;
+			var oldDay = date.getDay();
+			var index = (days.indexOf(oldDay) + 1) % days.length;
+			var daysDiff = days[index] - oldDay;
+			if (daysDiff < 0)
+				daysDiff += 7;
+			date.setDate(date.getDate() + daysDiff);
+		}
+		$("#repeat-modal").modal("hide");
 	});
 });
