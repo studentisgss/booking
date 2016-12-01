ac9841187878987ea3034c8fe12b9f20c981797b
Daniele Troletti <dan.troletti@gmail.com>
2016-12-01 22:41:14 +0100

    Insert auth urls in urls.py instead of include them

--- authentication/urls.py
+++ authentication/urls.py
@@ -13,10 +13,22 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.conf.urls import url
+from django.contrib.auth import views
+from django.core.urlresolvers import reverse_lazy
 
 app_name = "authentication"
 
 urlpatterns = [
-    url('^', include('django.contrib.auth.urls')),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout'),
+    url(r'^password_change/$', views.password_change,
+        {"post_change_redirect": reverse_lazy("authentication:password_change_done")},
+        name='password_change'),
+    url(r'^password_change/done/$', views.password_change_done, name='password_change_done'),
+    url(r'^password_reset/$', views.password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', views.password_reset_complete, name='password_reset_complete'),
 ]
