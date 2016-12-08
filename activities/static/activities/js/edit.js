3bec9fbdb5fabef6fb676ec7b950e680657f6193
Daniele Troletti <dan.troletti@gmail.com>
2016-12-08 15:22:17 +0100

    Rewrite activity edit.js

--- activities/static/activities/js/edit.js
+++ activities/static/activities/js/edit.js
@@ -1,262 +1,256 @@
 // Variable for repeat function
 var repeatTarget = null;
 
+// HELPER FUNCTION
+function setWaitingWarning(tr) {
+    tr.addClass("warning");
+    tr.attr("title", "Questa prenotazione verrà messa in attesa di approvazione.");
+    var select = $("select[name*='status']", tr);
+    if (select.val() == 0) {
+        select.val(1);
+    }
+    $("option[value=0]", select).remove();
+}
+
+function removeWaitingWarning(tr) {
+    tr.removeClass("warning");
+    tr.attr("title", "");
+    var valOriginal = $("select[name*='status']", tr).val();
+    $("select[name*='status']", tr).html(
+        $("select[name*='status']", $("#tr-empty")).html()
+    );
+    $("select[name*='status']", tr).val(valOriginal);
+}
+
+function setNoPermissionWarning(tr) {
+    tr.addClass("danger");
+    tr.attr("title", "Non si possiede nessun permesso su quest'aula. Per poter modificare questa prenotazione si deve selezionare un'altra aula.");
+    $("input", tr).prop("readonly", true);
+    $("button > span.glyphicon-retweet", tr).parent().prop("disabled", true);
+    var status = $("select[name*='status']", tr);
+    $("option:not(:selected)", status).each(function() {
+        $(this).remove();
+    });
+}
+
+function removeNoPermissionWarning(tr) {
+    tr.removeClass("danger");
+    $("input", tr).prop("readonly", false);
+    $("button > span.glyphicon-retweet", tr).parent().prop("disabled", false);
+    $("option", this).filter(function(i, el){
+        return (allRooms.indexOf(el.value) == -1) && (el.value != "");
+    }).remove();
+    var valOriginal = $("select[name*='status']", tr).val();
+    $("select[name*='status']", tr).html(
+        $("select[name*='status']", $("#tr-empty")).html()
+    );
+    $("select[name*='status']", tr).val(valOriginal);
+}
+
+function addForm(element, before = false) {
+    var id = $("#id_event_set-TOTAL_FORMS").val();
+    $("#id_event_set-TOTAL_FORMS").val(parseInt(id) + 1);
+    var el = $($(element)[0].outerHTML.replace(/__prefix__/g, id));
+    el.removeAttr("id");
+    if (before) {
+        // Find the last filled form
+        var elementAfter = $("tr").filter(function() {
+            var tr = $(this);
+            var filled = false;
+            $("select", tr).each(function() {
+                filled |= $(this).val() != "";
+            });
+            if (filled) { // If the form is already filled do not go further
+                return filled;
+            }
+            $("input[type='text']", tr).each(function() {
+                filled |= $(this).val() != "";
+            });
+            return filled;
+        }).last();
+        el.insertAfter(elementAfter);
+    }
+    else {
+        el.insertBefore($('#tr-button'));
+    }
+    el.removeClass("hidden");
+    $(".datetime:even", el).removeClass("hasDatepicker");
+    $(".datetime:even", el).datepicker();
+    return el;
+}
+
+function clearModal() {
+    var modal = $("#repeat-modal");
+    $("input:not([type=\"checkbox\"])", modal).val("");
+    $("input[type=\"checkbox\"]", modal).prop("checked", false);
+}
+
+function checkInput(obj) {
+    var tr = $(obj).parent().parent();
+    if ($(obj).hasClass("hasDatepicker")) {
+        $(obj).trigger("change");
+    }
+    var select = $("select[name*='status']", tr);
+    var roomVal = $("select[name*='room']", tr).val();
+    if (($("select[name*='room']", tr).data("original-value") != roomVal) ||
+            ($("select[name*='status']", tr).data("original-value") != 0)) {
+        return;
+    }
+    var inputs = $("input", tr);
+    var originalValue = true;
+    inputs.each(function() {
+        originalValue = originalValue && ($(this).data("original-value") == this.value);
+    });
+    if (originalValue) {
+        removeWaitingWarning(tr);
+        $("select[name*='status']", tr).val(0);
+    }
+    else {
+        setWaitingWarning(tr);
+    }
+}
+
+// DOCUMENT READY
 $(document).ready(function() {
-	// Highlight event which cannot be approved
-	$(document).on("change", "select[name*='room'].form-control", function(){
-		var tr = $(this).parent().parent();
-		if (waitingRooms.indexOf(this.value) > -1)
-		{
-			tr.addClass("warning");
-			tr.attr("title", "Questa prenotazione verrà messa in attesa di approvazione.");
-			var select = $("select[name*='status']", tr);
-			if (select.val() == 0)
-			{
-				select.val(1);
-			}
-			$("option[value=0]", select).remove();
-			if ($(this).data("original-value"))
-			{
-				checkInput(this);
-			}
+    // Highlight event which cannot be approved
+    $(document).on("change", "select[name*='room'].form-control", function() {
+        var tr = $(this).parent().parent();
+        if (waitingRooms.indexOf(this.value) > -1) {
+            setWaitingWarning(tr);
+            if ($(this).data("original-value")) {
+                checkInput(this);
+            }
+        }
+        else {
+            removeWaitingWarning(tr);
+        }
+    });
 
-		}
-		else
-		{
-			tr.removeClass("warning");
-			tr.attr("title", "");
-			var valOriginal = $("select[name*='status']", tr).val();
-			$("select[name*='status']", tr).html(
-				$("select[name*='status']", $("#tr-empty")).html()
-			);
-			$("select[name*='status']", tr).val(valOriginal);
-		}
-	});
+    // Enhance delete
+    var span = $("span.glyphicon-trash");
+    span.parent().removeClass("hidden");
+    span.parent().siblings("input").addClass("hidden");
 
-	// Enhance delete
-	var span = $("span.glyphicon-trash");
-	span.parent().removeClass("hidden");
-	span.parent().siblings("input").addClass("hidden");
-	$(document).on("click", "button.remover", function(){
-		$(this).siblings("input").prop("checked", true);
-		$(this).parent().parent().fadeOut();
-	});
+    $(document).on("click", "button.remover", function() {
+        $(this).siblings("input").prop("checked", true);
+        $(this).parent().parent().fadeOut();
+    });
 
-	// Set the dates of start and end automatically
-	$(document).on("change", ".datetime:even", function(){
-		tr = $(this).parent().parent();
-		$(".datetime:even", tr).val($(this).val());
-	});
+    // Set the dates of start and end automatically
+    $(document).on("change", ".datetime:even", function() {
+        tr = $(this).parent().parent();
+        $(".datetime:even", tr).val($(this).val());
+    });
 
-	function AddForm(element, before = false) {
-		var id = $("#id_event_set-TOTAL_FORMS").val();
-		$("#id_event_set-TOTAL_FORMS").val(parseInt(id) + 1);
-		var el = $($(element)[0].outerHTML.replace(/__prefix__/g, id));
-		el.removeAttr("id");
-		if (before)
-		{
-			// Find the last filled form
-			var elementAfter = $("tr").filter(function()
-			{
-				var tr = $(this);
-				var filled = false;
-				$("select", tr).each(function(){
-					filled |= $(this).val() != "";
-				});
-				if (filled){ // If the form is already filled do not go further
-					return filled;
-				}
-				$("input[type='text']", tr).each(function(){
-					filled |= $(this).val() != "";
-				});
-				return filled;
-			}).last();
-			el.insertAfter(elementAfter);
-		}
-		else
-		{
-			el.insertBefore($('#tr-button'));
-		}
-		el.removeClass("hidden");
-		$(".datetime:even", el).removeClass("hasDatepicker")
-		$(".datetime:even", el).datepicker();
-		return el;
-	}
+    // Add button
+    $("#tr-button").removeClass("hidden");
+    $("#add-button").on("click", function() {
+        addForm($('#tr-empty'));
+    });
 
-	// Add button
-	$("#tr-button").removeClass("hidden");
-	$("#add-button").on("click", function(){
-		AddForm($('#tr-empty'));
-	});
+    //REPEAT FUNCTION
+    $("button[data-target=\"#repeat-modal\"]").removeClass("hidden");
+    $("th").removeClass("hidden");
 
-	//REPEAT FUNCTION
-	$("button[data-target=\"#repeat-modal\"]").removeClass("hidden");
-	$("th").removeClass("hidden");
+    $("#repeat-modal").on("show.bs.modal", function(e) {
+        clearModal();
+        var btn = $(e.relatedTarget);
+        var tr = btn.parent().parent();
+        repeatTarget = tr;
+        var date = $(".datetime:first", tr).val();
+        if (date) {
+            dateSplit = date.split("/");
+            var d = new Date(dateSplit[2], dateSplit[1] - 1, dateSplit[0]);
+            d.setDate(d.getDate() + 1);
+            $("#date-from", $(this)).val(d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear());
+        }
+    });
 
-	function clearModal(){
-		var modal = $("#repeat-modal");
-		$("input:not([type=\"checkbox\"])", modal).val("");
-		$("input[type=\"checkbox\"]", modal).prop("checked", false);
-	}
+    $("#btn-repeat").on("click", function() {
+        var modal = $("#repeat-modal");
+        var days = $("input[name='day']:checked", modal).map(function(index,domElement) {
+            return parseInt($(domElement).val());
+        }).get();
+        days.sort();
+        var startDate = $("#date-from", modal).val();
+        var times = $("#repeat-times", modal).val();
+        if (!(days.length > 0 && startDate && times)) {
+            $("#repeat-form", modal).addClass("has-error");
+            return;
+        }
 
-	$("#repeat-modal").on("show.bs.modal", function(e){
-		clearModal();
-		var btn = $(e.relatedTarget);
-		var tr = btn.parent().parent();
-		repeatTarget = tr;
-		var date = $(".datetime:first", tr).val();
-		if (date){
-			dateSplit = date.split("/");
-			var d = new Date(dateSplit[2], dateSplit[1] - 1, dateSplit[0]);
-			d.setDate(d.getDate() + 1);
-			$("#date-from", $(this)).val(d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear());
-		}
-	});
+        var dateSplit = startDate.split("/");
+        var date = new Date(dateSplit[2], dateSplit[1] - 1, dateSplit[0]);
 
-	$("#btn-repeat").on("click", function(){
-		var modal = $("#repeat-modal");
-		var days = $("input[name='day']:checked", modal).map(function(index,domElement) {
-		    return parseInt($(domElement).val());
-		}).get();
-		days.sort();
-		var startDate = $("#date-from", modal).val();
-		var times = $("#repeat-times", modal).val();
-		if (!(days.length > 0 && startDate && times)){
-			$("#repeat-form", modal).addClass("has-error");
-			return;
-		}
+        // Get the first day greater or equal than date in days
+        while (days.indexOf(date.getDay()) == -1) {
+            date.setDate(date.getDate() + 1);
+        }
 
-		var dateSplit = startDate.split("/");
-		var date = new Date(dateSplit[2], dateSplit[1] - 1, dateSplit[0]);
+        var event = null;
+        var room = null;
+        var status = null;
+        while (times > 0) {
+            // Add the new event
+            event = addForm($('#tr-empty'), true);
+            event.removeAttr("id");
+            room = $("select[name*='room']", repeatTarget).val();
+            $("select[name*='room']", event).val(room);
+            status = $("select[name*='status']", repeatTarget).val();
+            if (status == 0 && waitingRooms.indexOf(room) > -1) {
+                setWaitingWarning(event);
+            }
+            else {
+                $("select[name*='status']", event).val(status);
+            }
 
-		// Get the first day greater or equal than date in days
-		while (days.indexOf(date.getDay()) == -1) {
-			date.setDate(date.getDate() + 1);
-		}
+            $.each($(".datetime:odd", event), function(i, value) {
+                $(value).val($(".datetime:odd", repeatTarget)[i].value);
+            });
+            $.each($(".datetime:even", event), function(i, value) {
+                $(value).val(date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear());
+            });
 
-		while (times > 0) {
-			// Add the new event
-			var event = AddForm($('#tr-empty'), true);
-			event.removeAttr("id");
-			var room = $("select", repeatTarget).val();
-			$("select[name*='room']", event).val(room);
-			var status = $("select[name*='status']", repeatTarget).val();
-			if (status == 0 && waitingRooms.indexOf(room) > -1)
-			{
-				var select =  $("select[name*='status']", event);
-				select.val(1);
-				$("option[value=0]", select).remove();
-				tr = select.parent().parent();
-				tr.addClass("warning");
-				tr.attr("title", "Questa prenotazione verrà messa in attesa di approvazione.");
-			}
-			else
-			{
-				$("select[name*='status']", event).val(status);
-			}
+            // Find next date and decrement times
+            times -= 1;
+            var oldDay = date.getDay();
+            var index = (days.indexOf(oldDay) + 1) % days.length;
+            var daysDiff = days[index] - oldDay;
+            if (daysDiff <= 0) {
+                daysDiff += 7;
+            }
+            date.setDate(date.getDate() + daysDiff);
+        }
+        $("#repeat-modal").modal("hide");
+    });
 
-			$.each($(".datetime:odd", event), function(i, value){
-				$(value).val($(".datetime:odd", repeatTarget)[i].value);
-			});
-			$.each($(".datetime:even", event), function(i, value){
-				$(value).val(date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear());
-			});
-
-			// Find next date and decrement times
-			times -= 1;
-			var oldDay = date.getDay();
-			var index = (days.indexOf(oldDay) + 1) % days.length;
-			var daysDiff = days[index] - oldDay;
-			if (daysDiff <= 0)
-				daysDiff += 7;
-			date.setDate(date.getDate() + daysDiff);
-		}
-		$("#repeat-modal").modal("hide");
-	});
-
-	function checkInput(obj){
-		var tr = $(obj).parent().parent();
-		if ($(obj).hasClass("hasDatepicker"))
-		{
-			$(obj).trigger("change");
-		}
-		var select = $("select[name*='status']", tr);
-		var roomVal = $("select[name*='room']", tr).val();
-		if ($("select[name*='room']", tr).data("original-value") != roomVal)
-		{
-			return;
-		}
-		var inputs = $("input", tr);
-		var originalValue = true;
-		inputs.each(function(){
-			originalValue = originalValue && ($(this).data("original-value") == this.value)
-		})
-		if (originalValue)
-		{
-			$("select[name*='status']", tr).html(
-				$("select[name*='status']", $("#tr-empty")).html()
-			);
-			$("select[name*='status']", tr).val(0);
-			tr.removeClass("warning");
-			tr.attr("title", "");
-		}
-		else
-		{
-			select.val(1);
-			$("option[value=0]", select).remove();
-			tr.addClass("warning");
-			tr.attr("title", "Questa prenotazione verrà messa in attesa di approvazione.");
-		}
-	}
-
-
-	// Room without any permission and set initial status
-	$("select[name*='room'].form-control").each(function(){
-		if ((allRooms.indexOf(this.value) == -1) && (this.value != ""))
-		{
-			var tr = $(this).parent().parent();
-			tr.addClass("danger");
-			tr.attr("title", "Non si possiede nessun permesso su quest'aula. Per poter modificare questa prenotazione si deve selezionare un'altra aula.");
-			$("input", tr).prop("readonly", true);
-			$("button > span.glyphicon-retweet", tr).parent().prop("disabled", true);
-			var status = $("select[name*='status']", tr);
-			$("option:not(:selected)", status).each(function(){
-				$(this).remove();
-			});
-			$(this).on("change", function(){
-				var tr = $(this).parent().parent();
-				tr.removeClass("danger");
-				$("input", tr).prop("readonly", false);
-				$("button > span.glyphicon-retweet", tr).parent().prop("disabled", false);
-				$("option", this).filter(function(i, el){
-					return (allRooms.indexOf(el.value) == -1) && (el.value != "");
-				}).remove();
-				var valOriginal = $("select[name*='status']", tr).val();
-				$("select[name*='status']", tr).html(
-					$("select[name*='status']", $("#tr-empty")).html()
-				);
-				$("select[name*='status']", tr).val(valOriginal);
-			});
-		}
-		if ((this.value != "") && (waitingRooms.indexOf(this.value) > -1))
-		{
-			var tr = $(this).parent().parent();
-			var select = $("select[name*='status']", tr);
-			if (select.val() != 0)
-			{
-				$("option[value=0]", select).remove();
-			}
-			if (select.val() == 1)
-			{
-				tr.addClass("warning");
-				tr.attr("title", "Questa prenotazione è in attesa di approvazione.");
-			}
-			$("input", tr).each(function(){
-				$(this).data("original-value", this.value);
-			});
-			$("select[name*='room']", tr).data("original-value", $("select[name*='room']", tr).val());
-			$("input:not(.hasDatepicker)", tr).on("change", function(){ checkInput(this); });
-			$("input.hasDatepicker", tr).datepicker("option", "onSelect", function(){ checkInput(this); });
-		}
-	});
+    // Room without any permission and set initial status
+    $("select[name*='room'].form-control").each(function() {
+        if ((allRooms.indexOf(this.value) == -1) && (this.value != "")) {
+            var tr = $(this).parent().parent();
+            setNoPermissionWarning(tr);
+            $(this).on("change", function() {
+                var tr = $(this).parent().parent();
+                removeNoPermissionWarning(tr);
+            });
+        }
+        if ((this.value != "") && (waitingRooms.indexOf(this.value) > -1)) {
+            var tr = $(this).parent().parent();
+            var select = $("select[name*='status']", tr);
+            if (select.val() != 0) {
+                $("option[value=0]", select).remove();
+            }
+            if (select.val() == 1) {
+                setWaitingWarning(tr);
+            }
+            $("input", tr).each(function() {
+                $(this).data("original-value", this.value);
+            });
+            $("select", tr).each(function() {
+                $(this).data("original-value", this.value);
+            });
+            $("input:not(.hasDatepicker)", tr).on("change", function(){ checkInput(this); });
+            $("input.hasDatepicker", tr).datepicker("option", "onSelect", function(){ checkInput(this); });
+        }
+    });
 });
