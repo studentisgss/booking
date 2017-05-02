a3ba95322485b907ea2f4f5a57d88c733d7abbf1
FedericoSold <FedericoSold@gmail.com>
2017-05-02 21:35:34 +0200

    only logged-in can see link

--- base/context_processors.py
+++ base/context_processors.py
@@ -3,3 +3,6 @@ from django.conf import settings
 
 def demo(request):
     return {'DEMO': settings.DEMO}
+
+def authenticated(request):
+    return {'AU':request.user.is_authenticated()}
--- booking/settings.py
+++ booking/settings.py
@@ -75,7 +75,8 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.i18n',
-                'base.context_processors.demo'
+                'base.context_processors.demo',
+		        'base.context_processors.authenticated'
             ],
         },
     },
