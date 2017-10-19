ff6722968e0d38df25ed449bad3760cf6f3d0f05
Daniele Troletti <dan.troletti@gmail.com>
2017-10-19 13:58:35 +0200

    Use class-based auth view
    Use the new class-based authentication views instead
    of the old and now deprecated function-based view.

--- authentication/urls.py
+++ authentication/urls.py
@@ -20,19 +20,22 @@ from django.core.urlresolvers import reverse_lazy
 app_name = "authentication"
 
 urlpatterns = [
-    url(r'^login/$', views.login, name='login'),
-    url(r'^logout/$', views.logout, name='logout'),
-    url(r'^password_change/$', views.password_change,
-        {"post_change_redirect": reverse_lazy("authentication:password_change_done")},
+    url(r'^login/$', views.LoginView.as_view(), name='login'),
+    url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
+    url(r'^password_change/$', views.PasswordChangeView.as_view(
+        success_url=reverse_lazy("authentication:password_change_done")),
         name='password_change'),
-    url(r'^password_change/done/$', views.password_change_done, name='password_change_done'),
-    url(r'^password_reset/$', views.password_reset,
-        {"post_reset_redirect": reverse_lazy("authentication:password_reset_done")},
+    url(r'^password_change/done/$', views.PasswordChangeDoneView.as_view(),
+        name='password_change_done'),
+    url(r'^password_reset/$', views.PasswordResetView.as_view(
+        success_url=reverse_lazy("authentication:password_reset_done")),
         name='password_reset'),
-    url(r'^password_reset/done/$', views.password_reset_done, name='password_reset_done'),
+    url(r'^password_reset/done/$', views.PasswordResetDoneView.as_view(),
+        name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        views.password_reset_confirm,
-        {"post_reset_redirect": reverse_lazy("authentication:password_reset_complete")},
+        views.PasswordResetConfirmView.as_view(
+            success_url=reverse_lazy("authentication:password_reset_complete")),
         name='password_reset_confirm'),
-    url(r'^reset/done/$', views.password_reset_complete, name='password_reset_complete'),
+    url(r'^reset/done/$', views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete'),
 ]
