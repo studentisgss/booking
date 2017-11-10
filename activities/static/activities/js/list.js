337954547df1df741a841ec651e528c4af534bb4
Daniele Troletti <dan.troletti@gmail.com>
2017-11-10 23:12:56 +0100

    Add searching to the list of activities

--- activities/static/activities/js/list.js
+++ activities/static/activities/js/list.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-    $("#table_events").DataTable({
+    dataTable = $("#table_events").DataTable({
         searching: false,
         paging: false,
         info: false,
         "columnDefs": [
-            { "orderable": false, "targets": 4 }
+            { "orderable": false, "targets": 4 },
         ]
     });
 });
