4554becc481d3be4501ad1509530d2d7319e04af
Daniele Troletti <dan.troletti@gmail.com>
2017-11-17 21:16:17 +0100

    Prevent user logged in with SSO to change password

--- authentication/urls.py
+++ authentication/urls.py
@@ -26,20 +26,20 @@ urlpatterns = [
         name='login-local'),
     url(r'^login/sso/$', LoginSSO.as_view(), name='login-sso'),
     url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
-    url(r'^password_change/$', views.PasswordChangeView.as_view(
+    url(r'^password_change/$', BookingPasswordChangeView.as_view(
         success_url=reverse_lazy("authentication:password_change_done")),
         name='password_change'),
-    url(r'^password_change/done/$', views.PasswordChangeDoneView.as_view(),
+    url(r'^password_change/done/$', BookingPasswordChangeDoneView.as_view(),
         name='password_change_done'),
-    url(r'^password_reset/$', views.PasswordResetView.as_view(
+    url(r'^password_reset/$', BookingPasswordResetView.as_view(
         success_url=reverse_lazy("authentication:password_reset_done")),
         name='password_reset'),
-    url(r'^password_reset/done/$', views.PasswordResetDoneView.as_view(),
+    url(r'^password_reset/done/$', BookingPasswordResetDoneView.as_view(),
         name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        views.PasswordResetConfirmView.as_view(
+        BookingPasswordResetConfirmView.as_view(
             success_url=reverse_lazy("authentication:password_reset_complete")),
         name='password_reset_confirm'),
-    url(r'^reset/done/$', views.PasswordResetCompleteView.as_view(),
+    url(r'^reset/done/$', BookingPasswordResetCompleteView.as_view(),
         name='password_reset_complete'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,9 +1,22 @@
 from django.views.generic import TemplateView
 from django.http import Http404, HttpResponseRedirect
+from django.contrib.auth.mixins import AccessMixin
+from django.contrib.auth import views as auth_views
 
 from django.shortcuts import render
 
 from booking.settings import LOGIN_REDIRECT_URL
+
+
+class NotShibbolethMixin(AccessMixin):
+    """Verify that the current user is NOT authenticated through Shibboleth."""
+    raise_exception = True
+    permission_denied_message = "Operazione non consentita."
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.session.get("AUTH_SHIB", False):
+            return self.handle_no_permission()
+        return super().dispatch(request, *args, **kwargs)
 
 
 class LoginSelector(TemplateView):
@@ -21,6 +34,32 @@ class LoginSSO(TemplateView):
     def get(self, request):
         # If the user is logged in redirect to the correct page
         if request.user.is_authenticated:
+            # Mark the user as logged using SSO
+            request.session["AUTH_SHIB"] = True
             nextUrl = request.GET.get("next", LOGIN_REDIRECT_URL)
             return HttpResponseRedirect(nextUrl)
         return super().get(request)
+
+
+class BookingPasswordResetView(NotShibbolethMixin, auth_views.PasswordResetView):
+    pass
+
+
+class BookingPasswordResetDoneView(NotShibbolethMixin, auth_views.PasswordResetDoneView):
+    pass
+
+
+class BookingPasswordResetConfirmView(NotShibbolethMixin, auth_views.PasswordResetConfirmView):
+    pass
+
+
+class BookingPasswordResetCompleteView(NotShibbolethMixin, auth_views.PasswordResetCompleteView):
+    pass
+
+
+class BookingPasswordChangeView(NotShibbolethMixin, auth_views.PasswordChangeView):
+    pass
+
+
+class BookingPasswordChangeDoneView(NotShibbolethMixin, auth_views.PasswordChangeDoneView):
+    pass
