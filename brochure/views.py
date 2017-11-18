b3e4ecb6d32fbe6fe068040d52fa1c16070f6c3a
Roberto Albesiano <roberto.albesiano@gmail.com>
2017-11-18 19:49:42 +0100

    Full brochure (front + body)

--- brochure/views.py
+++ brochure/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView, View
 from django.http import Http404, HttpResponse
+from django.utils import timezone
 from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.template.loader import get_template
 
@@ -14,6 +15,8 @@ class BrochurePDFView(View):
 
     def get_context_data(self, category=None, **kwargs):
         context = {}
+
+        # context for body
         if category in self.CLASS_CHOICES:
             activities_list = Activity.objects \
                 .filter(archived = False, category = category) \
@@ -22,6 +25,21 @@ class BrochurePDFView(View):
         else:
             activities_list = []
         context["list"] = activities_list
+
+        # context for front page: academic year
+        today = timezone.now()
+        year = today.year
+        if today.month < 9:
+            year = year - 1
+        context["academic_year"] = str(year) + "/" + str(year+1)
+
+        # context for front page: class
+        CLASS_DICT = {
+            'SN': 'Natural Sciences',
+            'SM': 'Moral Sciences',
+            'SS': 'Social Sciences'
+        }
+        context["category"] = CLASS_DICT[category]
         return context
 
     def get(self, request, category=None, *args, **kwargs):
@@ -30,11 +48,23 @@ class BrochurePDFView(View):
         response = HttpResponse(content_type = 'application/pdf')
         context = self.get_context_data(category=category, **kwargs)
 
+        # body generation
         body_template = get_template('brochure/body.html')
         body_html = body_template.render(context, request)
-
-        body_css = request.build_absolute_uri(static('brochure/body.css'))
+        body_css = request.build_absolute_uri(static('css/body.css'))
         brochure = wp.HTML(string = body_html) \
             .render(stylesheets = [wp.CSS(url = body_css)])
+
+        # front page generation
+        front_template = get_template('brochure/front.html')
+        front_html = front_template.render(context, request)
+        front_css = request.build_absolute_uri(static('css/front.css'))
+        front = wp.HTML(string = front_html, \
+            base_url = request.build_absolute_uri()) \
+            .render(stylesheets = [wp.CSS(url = front_css)])
+
+        # add front page and print
+        brochure.pages.insert(0, front.pages[0])
         brochure.write_pdf(target = response)
+
         return response
