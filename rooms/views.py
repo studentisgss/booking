6c58b1b32e2579d99d9ed78c015e202ee601ba31
FedericoSold <FedericoSold@gmail.com>
2017-12-09 22:13:18 +0100

    remove old code and change views inheritanche

--- rooms/views.py
+++ rooms/views.py
@@ -74,7 +74,8 @@ class ListAllRoomView(TemplateView):
         return context
 
 
-class ListRoomView(ListAllRoomView):
+class ListRoomView(TemplateView):
+    template_name = "rooms/list.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
