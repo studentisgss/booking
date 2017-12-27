e285f348c5096bafa5408daab990951bf52d8c8e
FedericoSold <FedericoSold@gmail.com>
2017-12-27 22:18:24 +0100

    add js in room detail

---
+++ rooms/static/rooms/js/detail.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  // Show the map and hide the address
+  $("#text_address").remove();
+  $("#map_address").removeClass("hidden");
+});
---
+++ rooms/static/rooms/js/list.js
@@ -0,0 +1,5 @@
+// DOCUMENT READY
+$(document).ready(function() {
+    // Collapse and the reopen first news to collapse all the other
+     $("div.to_be_collapsed").addClass("collapse");
+ });
