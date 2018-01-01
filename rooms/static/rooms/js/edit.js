89babe31adf578169d469fd0369b417241b8cf64
FedericoSold <FedericoSold@gmail.com>
2018-01-01 21:46:02 +0100

    enhance delete roomrule with js 2

---
+++ rooms/static/rooms/js/edit.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    // Enhance delete
+    // if js is enabled substitute the button
+    var span = $("span.glyphicon-trash");
+    span.parent().removeClass("hidden");
+    span.parent().siblings("input").addClass("hidden");
+    // when the button is clicked then
+    // - check the form
+    // - hide the row of the table
+    $(document).on("click", "button.remover", function() {
+        $(this).siblings("input").prop("checked", true);
+        $(this).parent().parent().fadeOut();
+    });
+})
