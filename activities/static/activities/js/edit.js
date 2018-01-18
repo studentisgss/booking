93b1f76fefb469ebbc7b7134e87c30fcc0becab7
Daniele Troletti <dan.troletti@gmail.com>
2018-01-19 00:22:59 +0100

    Improve select multiple in edit activity

--- activities/static/activities/js/edit.js
+++ activities/static/activities/js/edit.js
@@ -126,6 +126,18 @@ $(document).ready(function() {
         }
     });
 
+    // Enhanced select control with jquery sumoselect
+
+    $("#id_managers").SumoSelect({
+    	placeholder: 'Selezionare i referenti',
+        captionFormat:'{0} selezionati',
+        captionFormatAllSelected:'{0} tutti selezionati',
+        search: true,
+        searchText: 'Cerca...',
+        noMatch: 'Nessuna corrispondenza per "{0}"',
+        locale: ['OK', 'Annulla', 'Seleziona tutti']
+	});
+
     // Enhance delete
     var span = $("span.glyphicon-trash");
     span.parent().removeClass("hidden");
