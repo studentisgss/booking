739f441352f1b827c316b684e51f265032b34b29
lzampieri <lzampieri>
2018-02-01 23:40:16 +0100

    Added pagination in javascript: it must be done also in html

--- activities/static/activities/js/search.js
+++ activities/static/activities/js/search.js
@@ -5,16 +5,18 @@ $(document).ready(function() {
     //Add DataTable
     if ($("#table_events th").length == 4) {
         dataTable = $("#table_events").DataTable({
-            paging: false,
+            paging: true,
             info: false,
+            "pageLength": 25,
             "columnDefs": [
                 { "searchable": false, "targets": 3 },
             ]
         });
     } else {
         dataTable = $("#table_events").DataTable({
-            paging: false,
+            paging: true,
             info: false,
+            "pageLength": 25,
             "columnDefs": [
                 { "orderable": false, "targets": 4 },
                 { "searchable": false, "targets": 3 },
@@ -24,8 +26,6 @@ $(document).ready(function() {
     }
     // Remove the search button
     $("#filter-form div.input-group-btn").remove();
-    // Remove the DataTable search box
-    $("#table_events_filter").parent().parent().remove();
     // Add the event handler
     // After the user type something it immediatly filter the table
     $("#search").on("keyup", function(){
