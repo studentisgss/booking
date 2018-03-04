0d25ebdf69fb6539c4f2dc406607dd3dfa55fa4a
lzampieri <lzampieri>
2018-03-04 12:58:22 +0100

    Change date sorting method to support all languages

--- base/static/js/datatables_custom.js
+++ base/static/js/datatables_custom.js
@@ -40,12 +40,6 @@ var monthDict = {
 }
 
 $.fn.dataTable.ext.type.order['formatteddate-pre'] = function ( d ) {
-  var spl = d.split(" ",6);
-  if(spl.length !== 5) return 0;
-  var startyy = parseInt(spl[1]) % 100;  // max 2 cifre
-  var endyy = parseInt(spl[4]) % 100; // max 2 cifre
-  var startmth = monthDict[spl[0]]; // max 2 cifre
-  var endmth = monthDict[spl[3]]; // max 2 cifre
-  var convint = (( endyy*100 + endmth) *100 + startyy ) *100 + startmth; // integer endyy-endmth-startyy-startmth
-  return convint;
+  var spl = d.split("|",3);
+  return parseInt(spl[1]);
 }
