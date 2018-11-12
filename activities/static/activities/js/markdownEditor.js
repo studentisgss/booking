1b55df776ffe77e9ba9b139c2f140b735c845ed6
Daniele Troletti <dan.troletti@gmail.com>
2018-11-13 00:01:36 +0100

    Use CKEditor as Markdown editor

--- activities/static/activities/js/markdownEditor.js
+++ activities/static/activities/js/markdownEditor.js
@@ -1,12 +1,17 @@
 // DOCUMENT READY
 $(document).ready(function() {
     if (!($("#id_description").prop("disabled"))) {
-        var simplemde = new SimpleMDE({ element: $("#id_description")[0],
-                                        forceSync: true,
-                                        toolbar: ["bold", "italic", "strikethrough", "|",
-                                                    "heading-2", "heading-3", "|",
-                                                    "quote", "unordered-list", "ordered-list", "|",
-                                                    "link", "table", "horizontal-rule", "|",
-                                                    "preview", "|", "guide"]});
+        // Load the editor
+        ClassicEditor.create(document.querySelector('#id_description'),
+            {
+                language: 'it',
+                toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote', '|', 'undo', 'redo' ],
+                plugin: 'Markdown'
+            })
+            .then(editor => {
+            })
+            .catch(error => {
+                console.error( error );
+            });
     }
 });
